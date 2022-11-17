9ee3d6734b69deb95fb681c96037cf6751afdb80
Mohamad Zuhaily Mahmud <mzuhailym@gmail.com>
2022-11-17 15:26:49 +0800

    formula review

--- TheoryTest_v1.py
+++ TheoryTest_v1.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 k = 1.380649*10**(-23)
-tempt = 300
-vel = np.arange(0,2500,1)
-mass16O = 2.6569e-26 #mass of a single atom in kg (not equal to atomic mass unit)
-mass4He = 6.6422e-27
+tempt = [300,400,500]
+vel = np.arange(0,2000,1)
+amu = 1.66e-27
+mass = 85*amu
+
 
 def theory(m,T,v):
   a = (m/(2*np.pi*k*T))**(3/2)
@@ -16,10 +17,11 @@ def theory(m,T,v):
   
 fig = plt.figure(figsize=(12,9))
 ax1=fig.add_subplot()
-ax1.plot(vel, theory(mass16O,tempt,vel), label = '16O', color = 'red', linestyle = '--') # plotting for theoretical value
-ax1.plot(vel, theory(mass4He,tempt,vel), label = '4He', color = 'blue', linestyle = '--') # plotting for theoretical value
+ax1.plot(vel, theory(mass,tempt[0],vel), label = '16O', color = 'red', linestyle = '--') # plotting for theoretical value
+ax1.plot(vel, theory(mass,tempt[1],vel), label = '16O', color = 'blue', linestyle = '--') # plotting for theoretical value
+ax1.plot(vel, theory(mass,tempt[2],vel), label = '16O', color = 'green', linestyle = '--') # plotting for theoretical value
 ax1.set_xlabel('Velocity')
 ax1.set_ylabel('Probablity')
 ax1.title.set_text('Simulation of Maxwell-Boltzman Distribution')
 ax1.legend()
-plt.show()
+fig.savefig("TheoryTest.pdf")
