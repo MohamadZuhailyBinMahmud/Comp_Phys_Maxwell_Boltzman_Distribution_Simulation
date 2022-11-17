69977f720173c6047c7abe535765cc740b1c56ea
Mohamad Zuhaily Mahmud <mzuhailym@gmail.com>
2022-11-17 15:26:15 +0800

    formula testing and progess on monte testing

--- TestMonte_v1.py
+++ TestMonte_v1.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import ROOT
 
 k = 1.380649*10**(-23)
 tempt = 300
-vel = np.arange(0,2500,1)
+vel = np.arange(0,2000,1)
 mass16O = 2.6569e-26 #mass of a single atom in kg (not equal to atomic mass unit)
 mass4He = 6.6422e-27
 
@@ -16,8 +15,22 @@ def theory(m,T,v):
   c = np.exp(-x)
   return a*b*c
 
+def monte():
+  
+  def chance(m,T,v):
+    a = (m/(2*np.pi*k*T))**(3/2)
+    b = 4*np.pi*v**(2)
+    x = (m*v**(2))/(2*k*T)
+    c = np.exp(-x)
+    x = 1
+    
+  n = 100
+  count = np.zeros(n)
+  r = random.random()
+  print(r)
+   
 #print(theory(mass4He,tempt,vel))
 
-x = int(random.random)
+x = random.random
 print(x)
 
